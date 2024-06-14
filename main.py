8895b8db6b7987eeb4e5ff5d10442f16a8a6e1f3
ai-ces <155421638+ai-ces@users.noreply.github.com>
2024-06-14 05:07:41 +0300

    import

---
+++ main.py
@@ -0,0 +1,9 @@
+import warnings
+warnings.filterwarnings("ignore")
+
+from PyQt5.QtWidgets import *
+
+import numpy as np
+import pandas as pd
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.figure import Figure
